82b3a71b3134e39977c33ef6c728a8c12929b286
jyoti <rails@kunalinfotech.net>
2014-06-09 13:23:16 +0530

    added index page design integration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,19 +7,7 @@ class UsersController < ApplicationController
 
   def index
 	   @users = User.where(:is_admin.exists => false).order("id DESC").paginate(page: params[:page], per_page: 10)
-	   @contacts = request.env['omnicontacts.contacts']
-       respond_to do |format|
-       format.html
-     end
-  end
-
-
-  def contact_callback
-    @contacts = request.env['omnicontacts.contacts']
-    @user = request.env['omnicontacts.user']
-    puts "List of contacts of #{user[:name]} obtained from #params[:importer]}:"
-    @contacts.each do|contact|puts "Contact found: name => #{contact[:name]}, email => #{contact[:email]}"
-    end
+	   
   end
 
 
