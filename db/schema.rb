96218f2c2d3c301b9230280069a604c2eb844eea
jyoti <rails@kunalinfotech.net>
2014-06-10 19:25:59 +0530

    changes in popup fields

--- db/schema.rb
+++ db/schema.rb
@@ -102,7 +102,7 @@ ActiveRecord::Schema.define(version: 20140610123143) do
     t.boolean  "guest"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
