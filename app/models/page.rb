ef3ebb579127673a34a47d412929eea103449529
jyoti <rails@kunalinfotech.net>
2014-06-10 17:23:06 +0530

    added changes in design buttons

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,5 @@
 class Page < ActiveRecord::Base
 	  attr_accessible :title, :body, :photo
-	  validates_presence_of :photo
 	  has_attached_file  :photo,:styles => { :thumb => "176x100", :medium => "480x270>", :profile => "130x126"}
 	  validates_attachment :photo, content_type: { content_type: ['image/jpeg', 'image/GIF'] }
 end
