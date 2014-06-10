172ed8ae4bcc51392273856be861ff24d0896b1b
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-06-10 18:23:08 +0530

    design changes

--- app/mailers/feedback_mailer.rb
+++ app/mailers/feedback_mailer.rb
@@ -1,11 +1,18 @@
 class FeedbackMailer < ActionMailer::Base
   default from: "from@example.com"
 
+  # def feedback_mailer(feedback)
+  #   @feedback = feedback
+  #   email4 = "priya11051988@gmail.com"
+  #   recipients =  email4
+  #   subject = "Feedback"
+  #   mail(:subject => 'Feedback', :to => recipients) 
+  # end 
+
   def feedback_mailer(feedback)
     @feedback = feedback
-    email4 = "priya11051988@gmail.com"
-    recipients =  email4
+   
     subject = "Feedback"
-    mail(:subject => 'Feedback', :to => recipients) 
+    mail(:subject => 'Feedback', :to => feedback.email) 
   end 
 end
