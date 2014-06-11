d3b424f01b93a602360c92ccaf24d8a5045bac8b
Erik Michaels-Ober <sferik@gmail.com>
2014-06-11 17:31:17 +0530

    omnicontact redirect path changed for gmail

--- config/initializers/omnicontacts.rb
+++ config/initializers/omnicontacts.rb
@@ -1,5 +1,5 @@
 require "omnicontacts"
  Rails.application.middleware.use OmniContacts::Builder do
-  importer :gmail, "881639063021-cna2s7l4sqot6vh1kf2s735vhi6gbl7d.apps.googleusercontent.com','9CnwRY9v0a1q-xd7civFE9D5", {:redirect_path => "/users/gmail/contact_callback"}
+  importer :gmail, "881639063021-cna2s7l4sqot6vh1kf2s735vhi6gbl7d.apps.googleusercontent.com','9CnwRY9v0a1q-xd7civFE9D5", {:redirect_path => "#{Rails.root}"}
   importer :facebook, "482089455256431", "44bc20584648cf3b8d3a1ec0c8798f83",{:redirect_path =>  "#{Rails.root}"}
 end
