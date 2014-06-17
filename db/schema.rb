cea1c82704528c218fce09698b62be663d23339c
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-06-17 15:47:00 +0530

    schema changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 20140616103935) do
+ActiveRecord::Schema.define(version: 20140617064834) do
 
   create_table "advice_contacts", force: true do |t|
     t.string   "email"
