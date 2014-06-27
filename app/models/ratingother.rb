0dba0b857d1c2819576753deeecb024db5ddd0b8
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-06-27 19:01:08 +0530

    changes

--- app/models/ratingother.rb
+++ app/models/ratingother.rb
@@ -1,5 +1,7 @@
 class Ratingother < ActiveRecord::Base
  attr_accessible :user_id,:email,:trustworthy, :kind_helpful, :potential,:perform_well,:presentable,:emotianally_mature,:friendly_social,:rate_count
  belongs_to  :user
+ validates_presence_of :email
+ validates_uniqueness_of :email
 
 end
