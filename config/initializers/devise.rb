0be46e066f8f2a756f39b4181e11f469493a5391
jyoti <rails@kunalinfotech.net>
2014-06-30 12:12:27 +0530

    changes in facebook api

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -260,11 +260,11 @@ Devise.setup do |config|
   
   #local api crediantials
 
-  config.omniauth :facebook, '276696835845012', '92509aff57114d1880ed8ef664ff30cb'
+  #config.omniauth :facebook, '276696835845012', '92509aff57114d1880ed8ef664ff30cb'
   #config.omniauth :google_oauth2, '390453749585-98jnem85olk2arqb9cdf8oor86q7p3iv.apps.googleusercontent.com','nglC40-gLAW3sUOrL_iWOPNh'
 
   # heroku api credintials
-  #config.omniauth :facebook, '1462181317357560', 'd91764ff892721bf8752f9ceb3da6e43'
-  #config.omniauth :google_oauth2, '881639063021-cna2s7l4sqot6vh1kf2s735vhi6gbl7d.apps.googleusercontent.com','9CnwRY9v0a1q-xd7civFE9D5'
-  config.omniauth :google_oauth2, '287999877117-1pqqnv47f6gl6m5h4nh5n7h9ua8if4hq.apps.googleusercontent.com','6fX4-ygPGze1bc571nc6qyI3'
+  config.omniauth :facebook, '651706351611014', '09bb2b426d1e494af255ad62569a47e9'
+  config.omniauth :google_oauth2, '881639063021-cna2s7l4sqot6vh1kf2s735vhi6gbl7d.apps.googleusercontent.com','9CnwRY9v0a1q-xd7civFE9D5'
+  
 end
