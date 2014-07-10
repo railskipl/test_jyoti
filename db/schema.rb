fc572991410e6a4b92736ec10f65478839ccbbf8
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-07-10 17:54:35 +0530

    Navigation changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709105830) do
-
+ActiveRecord::Schema.define(version: 20140709130435) do
 
   create_table "advice_contacts", force: true do |t|
     t.string   "email"
