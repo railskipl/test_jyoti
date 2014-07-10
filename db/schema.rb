e7f2660219241b003abf5740725c9457e6ea9f5a
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-07-10 10:51:15 +0530

    added tip response

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140709072440) do
+ActiveRecord::Schema.define(version: 20140709130435) do
 
   create_table "advice_contacts", force: true do |t|
     t.string   "email"
@@ -321,6 +321,8 @@ ActiveRecord::Schema.define(version: 20140709072440) do
     t.string   "name"
     t.string   "respond_for_tip"
     t.string   "response_for_tip"
+    t.integer  "tip_accept",       default: 0
+    t.integer  "tip_reject",       default: 0
   end
 
   create_table "trial_days", force: true do |t|
