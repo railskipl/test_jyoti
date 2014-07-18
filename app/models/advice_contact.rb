da2b502e9662312dd9bb0d237ddba2875a34ea39
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-07-18 12:33:23 +0530

    Contacts

--- app/models/advice_contact.rb
+++ app/models/advice_contact.rb
@@ -1,7 +1,8 @@
 class AdviceContact < ActiveRecord::Base
 	attr_accessible :email, :praise, :criticism, :helpful_tips,:user_id
-	validates_presence_of :praise,:criticism,:helpful_tips
 	has_many :tips
 	belongs_to :user
 	has_many :ratingothers
+
+	
 end
