9d91d16c2b2e87a612a0ddf8b306997d619967f5
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-08-06 18:16:04 +0530

    added some changes

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -1,6 +1,6 @@
 class Rating < ActiveRecord::Base
 	
-attr_accessible :user_id,:friend_id,:trustworthy, :kind_helpful, :potential,:perform_well,:presentable,:emotianally_mature,:friendly_social,:count
+attr_accessible :user_id,:friend_id,:trustworthy, :kind_helpful, :potential,:perform_well,:presentable,:emotianally_mature,:friendly_social,:count,:overall_impresions
 belongs_to  :user
 belongs_to  :friend, :class_name => 'User'
 
