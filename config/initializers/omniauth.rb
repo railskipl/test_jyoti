bed96970709760e3fc4a61ea7fb40e514c9dbbcf
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-08-08 18:53:59 +0530

    changes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -16,6 +16,6 @@ OmniAuth.config.logger = Rails.logger
 OmniAuth.config.full_host = "http://m360.herokuapp.com"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1463677190567218','f21b189f3362ed4ca8f9d3d88961449c'
-  provider :google_oauth2, '537798958660-e4a44el7n3sfs590h3hjm7gsqsjssbfk.apps.googleusercontent.com','PUc-fKZSgOE3aemtEw0e0noG'
+  provider :google_oauth2, '537798958660-e4a44el7n3sfs590h3hjm7gsqsjssbfk.apps.googleusercontent.com','PUc-fKZSgOE3aemtEw0e0noG',{:access_type => 'online', :approval_prompt => '', :client_id => 'm360.herokuapp.com'}
 end
  
