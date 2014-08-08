bfd70c6db640104cadbc3596f85c856219529d83
atishkunalinfotech <atish.lewate@kunalinfotech.net>
2014-08-08 15:00:05 +0530

    added updates to omniauth logic

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -7,9 +7,11 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
     
     if @user.present?
       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      check_db
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
+      check_db
       redirect_to new_user_registration_url
     end
   end
@@ -22,11 +24,24 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
  
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      check_db
       sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
+      check_db
       redirect_to new_user_registration_url
     end
+  end
+end
+
+def check_db
+  @access_reputation_tip = AccessReputationTip.where('user_id = ?', current_user.id) rescue nil
+  @status_check = StatusCheck.where('user_id = ?', current_user.id)[0] rescue nil
+  unless @access_reputation_tip.present?
+    AccessReputationTip.create(:user_id => current_user.id)
+  end
+  unless @status_check.present?
+    StatusCheck.create(:user_id => current_user.id)
   end
 end
 
