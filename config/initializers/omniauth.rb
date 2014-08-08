8c9d4e40222f54aa08b6895cd62518c94e444f39
mayuri122222 <mayurisupe@gmail.com>
2014-08-08 12:54:50 +0530

    gamil & facebook login

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -11,7 +11,7 @@
 
 # For heroku server keys
 OmniAuth.config.logger = Rails.logger
-OmniAuth.config.full_host = "http://localhost:3000"
+OmniAuth.config.full_host = "http://m360.herokuapp.com"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, '1463677190567218','f21b189f3362ed4ca8f9d3d88961449c'
   provider :google_oauth2, '881639063021-cna2s7l4sqot6vh1kf2s735vhi6gbl7d.apps.googleusercontent.com','9CnwRY9v0a1q-xd7civFE9D5'
