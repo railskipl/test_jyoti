bd4a18aa837deddcb01edbea052cf0bd000c9663
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-08-11 11:36:42 +0530

    gmail

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -19,19 +19,18 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
  # Google authentication
   def google_oauth2
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
- 
+    # You need to implement the method below in your model (e.g. app/models/user.rb)
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
     if @user.persisted?
-      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      check_db
-      sign_in_and_redirect @user, :event => :authentication
+      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+      # flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
-      check_db
       redirect_to new_user_registration_url
     end
   end
-end
 
 def check_db
   @access_reputation_tip = AccessReputationTip.where('user_id = ?', current_user.id) rescue nil
