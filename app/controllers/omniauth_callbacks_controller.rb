5b69ce20895a4f179df5e79643ee2e8021b12ccc
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-08-11 12:22:48 +0530

    gmail signin

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -18,37 +18,37 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
 
  # Google authentication
-#   def google_oauth2
-#     @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
+  def google_oauth2
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"], current_user)
  
-#     if @user.persisted?
-#       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-#       check_db
-#       sign_in_and_redirect @user, :event => :authentication
-#     else
-#       session["devise.google_data"] = request.env["omniauth.auth"]
-#       check_db
-#       redirect_to new_user_registration_url
-#     end
-#   end
-# end
-
-
-
-
-def google_oauth2
-    # You need to implement the method below in your model (e.g. app/models/user.rb)
-    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
-      # flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
-      # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      check_db
+      sign_in_and_redirect @user, :event => :authentication
     else
       session["devise.google_data"] = request.env["omniauth.auth"]
+      check_db
       redirect_to new_user_registration_url
     end
   end
+end
+
+
+
+
+# def google_oauth2
+#     # You need to implement the method below in your model (e.g. app/models/user.rb)
+#     @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+#     if @user.persisted?
+#       sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+#       set_flash_message(:notice, :success, :kind => "Google") if is_navigational_format?
+#       # flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+#       # sign_in_and_redirect @user, :event => :authentication #this will throw if @user is not activated
+#     else
+#       session["devise.google_data"] = request.env["omniauth.auth"]
+#       redirect_to new_user_registration_url
+#     end
+#   end
 
 
   
