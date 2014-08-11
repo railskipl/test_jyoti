ccf9a44deeb8621535f4ac578b81df5e29979bc3
kipl <kipl@kipl-Inspiron-620s.(none)>
2014-08-11 17:52:30 +0530

    Authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -66,12 +66,34 @@ def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
 
 
 
+ # def self.find_for_google_oauth2(auth, signed_in_resource=nil)
+ #    user = User.where(:provider => auth.provider, :uid => auth.uid).first
+ #    if user
+ #      return user
+ #    else
+ #      registered_user = User.where(:email => auth.info.email).first
+ #      if registered_user
+ #        return registered_user
+ #      else
+ #        user = User.create(name:auth.extra.raw_info.name,
+ #                            provider:auth.provider,
+ #                            uid:auth.uid,
+ #                            email:auth.info.email,
+ #                            password:Devise.friendly_token[0,20]
+ #                          )
+ #      end
+       
+ #    end
+ #  end
+
+
+
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
     data = access_token.info #access_token contain all the information about user provider.
     user = User.where(:email => data["email"],:provider => "Google").first
     unless user
-      user = User.create(first_name: data["first_name"],
+      user = User.create(first_name: data["name"],# if user in not present then create user email,name and provider.
                          email: data["email"],
                          provider: "Google",
                          password: Devise.friendly_token[0,20]
@@ -98,6 +120,29 @@ def self.find_for_facebook_oauth(access_token, signed_in_resource=nil)
   end
 end
 
+
+
+
+
+#   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
+#     data = access_token.info
+#     user = User.where(:provider => access_token.provider, :uid => access_token.uid ).first
+#     if user
+#       return user
+#     else
+#       registered_user = User.where(:email => access_token.info.email).first
+#       if registered_user
+#         return registered_user
+#       else
+#         user = User.create(name: data["name"],
+#           provider:access_token.provider,
+#           email: data["email"],
+#           uid: access_token.uid ,
+#           password: Devise.friendly_token[0,20],
+#         )
+#       end
+#    end
+# end
 
 
 
